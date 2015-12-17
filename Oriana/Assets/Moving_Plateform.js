e4c73c9f5f85c521c36033bf58f5823d3fb053d3
Bobi NGO <bngo@e1r10p10.42.fr>
2015-12-18 00:01:03 +0100

    kek de minuit

--- Oriana/Assets/Moving_Plateform.js
+++ Oriana/Assets/Moving_Plateform.js
@@ -1,17 +1,44 @@
-﻿var Key = Transform[];
+﻿var Key : Transform [];
 var Plateform : Transform;
 
-var Direction = true;
-
-var Speed = 0.0;
-var Pause = 0.0;
-function Start () {
-	
-	Plateform.transform.position = PosA.transform.position;
-
-}
+var Current_Key = 1;
+var step = 0.00;
+var goForward = true;
 
 function Update () {
 
-	Vector2.Lerp(
+	movePlateform();
+	oneDirection();
+
+}
+
+function movePlateform()
+{
+	if (Vector2.Distance(Plateform.transform.position, Key[Current_Key].transform.position) > 0.1)
+	{
+		step += 0.001;
+		Plateform.transform.position = Vector2.Lerp(Plateform.transform.position, Key[Current_Key].transform.position, step);
+	}
+	if (Vector2.Distance(Plateform.transform.position, Key[Current_Key].transform.position) < 0.1)
+	{
+		step = 0.00;
+		if (goForward)
+			Current_Key++;
+		if (!goForward)
+			Current_Key--;					
+	}
+}
+
+function oneDirection()
+{
+	if (goForward && (Current_Key + 1) >= Key.Length)
+			goForward = false;
+	else if (!goForward && (Current_Key - 1) < 0)
+			goForward = true;
+}
+
+function OnTriggerEnter2D(Col : Collision2D)
+{
+	if(Col.gameObject.tag == "Player")
+		Col.transform.parent = this.transform;
 }
