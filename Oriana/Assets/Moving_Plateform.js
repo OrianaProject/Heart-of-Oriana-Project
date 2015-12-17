a8da0207bada876fe032946838714adc1dc647a6
Bobi NGO <bngo@e1r10p6.42.fr>
2015-12-17 18:58:10 +0100

    kek

---
+++ Oriana/Assets/Moving_Plateform.js
@@ -0,0 +1,17 @@
+﻿var Key = Transform[];
+var Plateform : Transform;
+
+var Direction = true;
+
+var Speed = 0.0;
+var Pause = 0.0;
+function Start () {
+	
+	Plateform.transform.position = PosA.transform.position;
+
+}
+
+function Update () {
+
+	Vector2.Lerp(
+}
--- Oriana/Assets/Scripts/EndLevelScript.js
+++ Oriana/Assets/Scripts/EndLevelScript.js
@@ -10,12 +10,20 @@ var endPos2 : Transform;
 var P1 : Transform;
 var P2 : Transform;
 
-var ovni : Transform;
+var end_Box_content : GameObject;
+
+var timer = 0.00;
+
+function Start()
+{
+	end_Box_content.SetActive(false);
+}
 
 function Update()
 {
 	if (isInside)
 		movePlayer();
+
 }
 
 function OnTriggerEnter2D(coll : Collider2D) {
@@ -32,18 +40,31 @@ function OnTriggerEnter2D(coll : Collider2D) {
 
 function movePlayer()
 {
+	if (P1.gameObject.GetComponent.<BoxCollider2D>().size.y != 1)
+		P1.gameObject.GetComponent.<BoxCollider2D>().size.y = 1;
+	if (P2.gameObject.GetComponent.<BoxCollider2D>().size.y != 1)
+		P2.gameObject.GetComponent.<BoxCollider2D>().size.y = 1;
 	if ((Vector2.Distance(P1.transform.position, endPos1.transform.position) && Vector2.Distance(P2.transform.position, endPos2.transform.position)) > 0.5 && !isMoved)
 	{
-		P1.transform.position = Vector2.Lerp(	P1.transform.position, endPos1.transform.position, Speed);
+		P1.transform.position = Vector2.Lerp(P1.transform.position, endPos1.transform.position, Speed);
 		P2.transform.position = Vector2.Lerp(P2.transform.position, endPos2.transform.position, Speed);
 		Debug.Log("Movement");
 	}
 	else
-		{
+		{	
 			Debug.Log("Saut");
-			isMoved = true;
-			//Player1_movement.rb.velocity = new Vector2(0,5);
-			//Player2_movement.rb.velocity = new Vector2(0,5);
-			ovni.GetComponent.<Animator>().Play("Ovni_Come");
+			if (GameObject.Find("Player_UI"))
+				Destroy(GameObject.Find("Player_UI"));
+			timer += 0.05;
+			if (Camera.main.rect.width >= 0.50)
+				Camera.main.rect.width -= 0.01;
+			if (Camera.main.rect.width <= 0.50)
+				end_Box_content.SetActive(true);
+			if (timer >= 5.00)
+			{
+				timer = 0.00;
+				Player1_movement.rb.velocity = new Vector2(0,5);
+				Player2_movement.rb.velocity = new Vector2(0,5);
+			}
 		}
 }
